c4460c416bbef6d0cde0c29c04986d8cb5930aad
Tushar Gawande <124259935+tushargawande45@users.noreply.github.com>
2025-02-05 11:36:26 +0530

    Update sonarScan.groovy

--- vars/sonarScan.groovy
+++ vars/sonarScan.groovy
@@ -1,1 +1,11 @@
+def call(String projectName, String projectKey, String sonarServer) {
+    withSonarQubeEnv(sonarServer) {
+        sh """
+            ${tool 'sonar-scanner'}/bin/sonar-scanner \
+            -Dsonar.projectName=${projectName} \
+            -Dsonar.projectKey=${projectKey} \
+            -Dsonar.java.binaries=.
+        """
+    }
+}
 
