1745a2cc23c7b52e91760097a615f50765a54617
Tushar Gawande <124259935+tushargawande45@users.noreply.github.com>
2025-02-05 11:35:01 +0530

    Update gitCheckout.groovy

--- vars/gitCheckout.groovy
+++ vars/gitCheckout.groovy
@@ -1,1 +1,8 @@
+def call(String url, String credentialsId) {
+    checkout([
+        $class: 'GitSCM',
+        branches: [[name: '*/master']],
+        userRemoteConfigs: [[url: url, credentialsId: credentialsId]]
+    ])
+}
 
