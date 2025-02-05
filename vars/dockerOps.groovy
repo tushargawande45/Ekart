f6818d91415c87da80281a0267f73f11643065fe
Tushar Gawande <124259935+tushargawande45@users.noreply.github.com>
2025-02-05 11:37:18 +0530

    Update dockerOps.groovy

--- vars/dockerOps.groovy
+++ vars/dockerOps.groovy
@@ -1,1 +1,9 @@
+def buildAndPush(String imageName, String dockerfilePath) {
+    sh """
+        docker login -u tushargawande --password Tushar@123
+        docker build -t ${imageName} -f ${dockerfilePath} .
+        docker tag ${imageName} tushargawande/${imageName}:latest
+        docker push tushargawande/${imageName}:latest
+    """
+}
 
