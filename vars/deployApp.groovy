29f664eeb287d1d5fe08d452b1860429671e0b41
Tushar Gawande <124259935+tushargawande45@users.noreply.github.com>
2025-02-05 11:38:46 +0530

    Update deployApp.groovy

--- vars/deployApp.groovy
+++ vars/deployApp.groovy
@@ -1,1 +1,8 @@
+def call(String containerName, String port, String imageName) {
+    sh """
+        docker stop ${containerName} || true
+        docker rm ${containerName} || true
+        docker run -d --name ${containerName} -p ${port}:${port} ${imageName}:latest
+    """
+}
 
